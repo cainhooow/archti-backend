5dec6b79de3a1df599e6caadb0152588bf78d73c
cainhooow <augustocaio663@gmail.com>
2026-03-21 12:12:50 -0300

    create table company specialties

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,14 +1,3 @@
-pub use sea_orm_migration::prelude::*;
-
-
-pub struct Migrator;
-
-#[async_trait::async_trait]
-impl MigratorTrait for Migrator {
-    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![]
-    }
-}
 mod m20260321_041852_create_table_companies;
 mod m20260321_043556_create_table_users;
 mod m20260321_050225_create_table_permissions;
@@ -18,3 +7,14 @@ mod m20260321_142529_create_table_company_addresses;
 mod m20260321_144012_create_table_company_memberships;
 mod m20260321_144722_create_table_roles;
 mod m20260321_145219_create_table_plan_versions;
+mod m20260321_150920_create_table_specialties;
+pub use sea_orm_migration::prelude::*;
+
+pub struct Migrator;
+
+#[async_trait::async_trait]
+impl MigratorTrait for Migrator {
+    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+        vec![]
+    }
+}
---
+++ migration/src/m20260321_150920_create_table_specialties.rs
@@ -0,0 +1,45 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .create_table(
+                Table::create()
+                    .col(uuid(Specialtie::Id).primary_key())
+                    .col(ColumnDef::new(Specialtie::CompanyId).uuid().not_null())
+                    .col(ColumnDef::new(Specialtie::Name).string_len(120).not_null())
+                    .col(
+                        ColumnDef::new(Specialtie::CreatedAt)
+                            .timestamp()
+                            .default(Expr::current_timestamp())
+                            .not_null(),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        todo!();
+
+        manager
+            .drop_table(Table::drop().table("post").to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+#[sea_orm(table_name = "specialties")]
+pub enum Specialtie {
+    Id,
+    Table,
+    CompanyId,
+    Name,
+    CreatedAt,
+}
