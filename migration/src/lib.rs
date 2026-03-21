83a2ffdf83fd6b80f3a94ca966541e13d553d615
cainhooow <augustocaio663@gmail.com>
2026-03-21 14:50:31 -0300

    create table resources

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -16,6 +16,7 @@ mod m20260321_153334_create_table_service_catalog_items;
 mod m20260321_154602_create_table_service_expense_presets;
 mod m20260321_160001_create_table_service_order_status_setps;
 mod m20260321_161049_create_table_stock_products;
+mod m20260321_173415_create_table_resources;
 
 pub struct Migrator;
 
---
+++ migration/src/m20260321_173415_create_table_resources.rs
@@ -0,0 +1,115 @@
+use sea_orm_migration::{prelude::*, schema::*, sea_orm::dynamic::Column};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .create_table(
+                Table::create()
+                    .table(Resource::Table)
+                    .col(uuid(Resource::Id).primary_key())
+                    .col(ColumnDef::new(Resource::CompanyId).uuid().not_null())
+                    .col(ColumnDef::new(Resource::Code).string_len(60).not_null())
+                    .col(ColumnDef::new(Resource::Name).string_len(160).not_null())
+                    .col(
+                        ColumnDef::new(Resource::ResourceType)
+                            .string_len(80)
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(Resource::ControlType)
+                            .string_len(80)
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(Resource::Unit).string_len(40).not_null())
+                    .col(ColumnDef::new(Resource::Quantity).integer().not_null())
+                    .col(ColumnDef::new(Resource::Minimum).integer().not_null())
+                    .col(
+                        ColumnDef::new(Resource::ItemCondition)
+                            .string_len(80)
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(Resource::Status).string_len(60).not_null())
+                    .col(
+                        ColumnDef::new(Resource::Location)
+                            .string_len(120)
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(Resource::Supplier)
+                            .string_len(160)
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(Resource::Brand).string_len(120).not_null())
+                    .col(ColumnDef::new(Resource::Application).text().not_null())
+                    .col(ColumnDef::new(Resource::AssetTag).string_len(60).null())
+                    .col(
+                        ColumnDef::new(Resource::SerialNumber)
+                            .string_len(120)
+                            .null(),
+                    )
+                    .col(
+                        ColumnDef::new(Resource::AverageCostCents)
+                            .integer()
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(Resource::LastEntryAt).date().null())
+                    .col(ColumnDef::new(Resource::Notes).text().null())
+                    .col(
+                        ColumnDef::new(Resource::CreatedAt)
+                            .timestamp()
+                            .default(Expr::current_timestamp())
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(Resource::UpdatedAt)
+                            .timestamp()
+                            .default(Expr::current_timestamp())
+                            .not_null(),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        todo!();
+
+        manager
+            .drop_table(Table::drop().table("post").to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+#[sea_orm(table_name = "resources")]
+pub enum Resource {
+    Id,
+    Table,
+    CompanyId,
+    Code,
+    Name,
+    ResourceType,
+    ControlType,
+    Unit,
+    Quantity,
+    Minimum,
+    ItemCondition,
+    Status,
+    Location,
+    Supplier,
+    Brand,
+    Application,
+    AssetTag,
+    SerialNumber,
+    AverageCostCents,
+    LastEntryAt,
+    Notes,
+    CreatedAt,
+    UpdatedAt,
+}
