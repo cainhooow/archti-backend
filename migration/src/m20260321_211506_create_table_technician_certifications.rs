01d391131c11ca954243b9620473cee5b30c0838
cainhooow <augustocaio663@gmail.com>
2026-03-21 18:19:06 -0300

    create table technician certifications

---
+++ migration/src/m20260321_211506_create_table_technician_certifications.rs
@@ -0,0 +1,52 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(TechnicianCertification::Table)
+                    .col(
+                        uuid(TechnicianCertification::TechnicianId)
+                            .primary_key()
+                            .not_null(),
+                    )
+                    .col(
+                        uuid(TechnicianCertification::CertificationId)
+                            .primary_key()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(TechnicianCertification::CreatedAt)
+                            .timestamp()
+                            .default(Expr::current_timestamp())
+                            .not_null(),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(
+                Table::drop()
+                    .table(TechnicianCertification::Table)
+                    .to_owned(),
+            )
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+#[sea_orm(table_name = "technician_certifications")]
+pub enum TechnicianCertification {
+    Table,
+    TechnicianId,
+    CertificationId,
+    CreatedAt,
+}
