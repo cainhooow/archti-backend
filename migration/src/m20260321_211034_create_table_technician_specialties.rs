cadccc86b5e48d9c05c4bb33aa5df8f7e1136e75
cainhooow <augustocaio663@gmail.com>
2026-03-22 15:33:17 -0300

    Technician Specialties foreign keys

--- migration/src/m20260321_211034_create_table_technician_specialties.rs
+++ migration/src/m20260321_211034_create_table_technician_specialties.rs
@@ -1,11 +1,34 @@
 use sea_orm_migration::{prelude::*, schema::*};
 
+use crate::{
+    m20260321_150920_create_table_specialties::Specialtie,
+    m20260321_190530_create_table_technicians::Technician,
+};
+
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let mut technician_fk = ForeignKey::create()
+            .from(
+                TechnicianSpeciality::Table,
+                TechnicianSpeciality::TechnicianId,
+            )
+            .to(Technician::Table, Technician::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
+
+        let mut specialty_fk = ForeignKey::create()
+            .from(
+                TechnicianSpeciality::Table,
+                TechnicianSpeciality::SpecialtyId,
+            )
+            .to(Specialtie::Table, Specialtie::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
+
         manager
             .create_table(
                 Table::create()
@@ -25,6 +48,8 @@ impl MigrationTrait for Migration {
                             .default(Expr::current_timestamp())
                             .not_null(),
                     )
+                    .foreign_key(&mut technician_fk)
+                    .foreign_key(&mut specialty_fk)
                     .to_owned(),
             )
             .await
