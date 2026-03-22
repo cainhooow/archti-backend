25d326cdbcf3c788e4eddf6609e714978d7b6f6d
cainhooow <augustocaio663@gmail.com>
2026-03-22 20:40:43 -0300

    Quotes items foreign keys

--- migration/src/m20260322_040822_create_table_quote_items.rs
+++ migration/src/m20260322_040822_create_table_quote_items.rs
@@ -1,11 +1,28 @@
 use sea_orm_migration::{prelude::*, schema::*};
 
+use crate::{
+    m20260321_161049_create_table_stock_products::StockProduct,
+    m20260322_011150_create_table_quotes::Quote,
+};
+
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let mut quote_fk = ForeignKey::create()
+            .from(QuoteItem::Table, QuoteItem::QuoteId)
+            .to(Quote::Table, Quote::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
+
+        let mut stock_product_fk = ForeignKey::create()
+            .from(QuoteItem::Table, QuoteItem::StockProductId)
+            .to(StockProduct::Table, StockProduct::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
+
         manager
             .create_table(
                 Table::create()
@@ -26,6 +43,8 @@ impl MigrationTrait for Migration {
                             .not_null(),
                     )
                     .col(ColumnDef::new(QuoteItem::SortOrder).integer().not_null())
+                    .foreign_key(&mut quote_fk)
+                    .foreign_key(&mut stock_product_fk)
                     .to_owned(),
             )
             .await
