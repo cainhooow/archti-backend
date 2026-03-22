ad5e1a9b8606769dee7a1cebed9d2856a855041a
cainhooow <augustocaio663@gmail.com>
2026-03-22 14:58:17 -0300

    Company Feature Overrides foreign keys

--- migration/src/m20260321_182739_create_table_company_feature_overrides.rs
+++ migration/src/m20260321_182739_create_table_company_feature_overrides.rs
@@ -1,12 +1,44 @@
 use sea_orm_migration::{prelude::*, schema::*};
 
+use crate::{
+    m20260321_041852_create_table_companies::Company,
+    m20260321_141334_create_table_plan_features::PlanFeature,
+    m20260321_144012_create_table_company_memberships::CompanyMembership,
+};
+
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
+        let mut company_fk = ForeignKey::create()
+            .from(
+                CompanyFeatureOverride::Table,
+                CompanyFeatureOverride::CompanyId,
+            )
+            .to(Company::Table, Company::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
+
+        let mut plan_feature_fk = ForeignKey::create()
+            .from(
+                CompanyFeatureOverride::Table,
+                CompanyFeatureOverride::FeatureId,
+            )
+            .to(PlanFeature::Table, PlanFeature::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
+
+        let mut membership_fk = ForeignKey::create()
+            .from(
+                CompanyFeatureOverride::Table,
+                CompanyFeatureOverride::CreatedByMembershipId,
+            )
+            .to(CompanyMembership::Table, CompanyMembership::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
+
         manager
             .create_table(
                 Table::create()
@@ -51,6 +83,9 @@ impl MigrationTrait for Migration {
                             .default(Expr::current_timestamp())
                             .not_null(),
                     )
+                    .foreign_key(&mut company_fk)
+                    .foreign_key(&mut plan_feature_fk)
+                    .foreign_key(&mut membership_fk)
                     .to_owned(),
             )
             .await
