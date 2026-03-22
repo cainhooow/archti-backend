6a5bd44c84063921bb173fb47c0510f9d2998f52
cainhooow <augustocaio663@gmail.com>
2026-03-22 19:30:38 -0300

    Service order timeline events foreign keys

--- migration/src/m20260322_035544_create_table_service_order_timeline_events.rs
+++ migration/src/m20260322_035544_create_table_service_order_timeline_events.rs
@@ -1,11 +1,34 @@
 use sea_orm_migration::{prelude::*, schema::*};
 
+use crate::{
+    m20260321_160001_create_table_service_order_status_setps::ServiceOrderStatusStep,
+    m20260322_000414_create_table_service_orders::ServiceOrder,
+};
+
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let mut service_order_fk = ForeignKey::create()
+            .from(
+                ServiceOrderTimelineEvent::Table,
+                ServiceOrderTimelineEvent::ServiceOrderId,
+            )
+            .to(ServiceOrder::Table, ServiceOrder::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
+
+        let mut status_step_fk = ForeignKey::create()
+            .from(
+                ServiceOrderTimelineEvent::Table,
+                ServiceOrderTimelineEvent::StatusStepId,
+            )
+            .to(ServiceOrderStatusStep::Table, ServiceOrderStatusStep::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
+
         manager
             .create_table(
                 Table::create()
@@ -37,6 +60,8 @@ impl MigrationTrait for Migration {
                             .default(Expr::current_timestamp())
                             .not_null(),
                     )
+                    .foreign_key(&mut service_order_fk)
+                    .foreign_key(&mut status_step_fk)
                     .to_owned(),
             )
             .await
