2ce72d65269cadc2933bc7bdec69a93809f58cc9
cainhooow <augustocaio663@gmail.com>
2026-03-22 19:28:17 -0300

    Service order payments methods fk

--- migration/src/m20260322_035016_create_table_service_order_payment_methods.rs
+++ migration/src/m20260322_035016_create_table_service_order_payment_methods.rs
@@ -1,11 +1,34 @@
 use sea_orm_migration::{prelude::*, schema::*};
 
+use crate::{
+    m20260321_151931_create_table_payment_methods::PaymentMethod,
+    m20260322_000414_create_table_service_orders::ServiceOrder,
+};
+
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let mut service_order_fk = ForeignKey::create()
+            .from(
+                ServiceOrderPaymentMethod::Table,
+                ServiceOrderPaymentMethod::ServiceOrderId,
+            )
+            .to(ServiceOrder::Table, ServiceOrder::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
+
+        let mut payment_method_fk = ForeignKey::create()
+            .from(
+                ServiceOrderPaymentMethod::Table,
+                ServiceOrderPaymentMethod::PaymentMethodId,
+            )
+            .to(PaymentMethod::Table, PaymentMethod::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
+
         manager
             .create_table(
                 Table::create()
@@ -26,6 +49,8 @@ impl MigrationTrait for Migration {
                             .integer()
                             .not_null(),
                     )
+                    .foreign_key(&mut service_order_fk)
+                    .foreign_key(&mut payment_method_fk)
                     .to_owned(),
             )
             .await
@@ -33,7 +58,11 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .drop_table(Table::drop().table(ServiceOrderPaymentMethod::Table).to_owned())
+            .drop_table(
+                Table::drop()
+                    .table(ServiceOrderPaymentMethod::Table)
+                    .to_owned(),
+            )
             .await
     }
 }
