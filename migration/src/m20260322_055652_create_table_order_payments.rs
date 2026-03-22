f0779cc9c027161169c7b5a2c21414f6858ad980
cainhooow <augustocaio663@gmail.com>
2026-03-22 20:49:51 -0300

    Order payments foreign keys

--- migration/src/m20260322_055652_create_table_order_payments.rs
+++ migration/src/m20260322_055652_create_table_order_payments.rs
@@ -1,11 +1,28 @@
 use sea_orm_migration::{prelude::*, schema::*};
 
+use crate::{
+    m20260321_151931_create_table_payment_methods::PaymentMethod,
+    m20260322_051337_create_table_orders::Order,
+};
+
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let mut order_fk = ForeignKey::create()
+            .from(OrderPayment::Table, OrderPayment::OrderId)
+            .to(Order::Table, Order::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
+
+        let mut payment_method_fk = ForeignKey::create()
+            .from(OrderPayment::Table, OrderPayment::PaymentMethodId)
+            .to(PaymentMethod::Table, PaymentMethod::Id)
+            .on_delete(ForeignKeyAction::Cascade)
+            .to_owned();
+
         manager
             .create_table(
                 Table::create()
@@ -25,6 +42,8 @@ impl MigrationTrait for Migration {
                     )
                     .col(ColumnDef::new(OrderPayment::ReceivedAt).timestamp().null())
                     .col(ColumnDef::new(OrderPayment::Note).text().null())
+                    .foreign_key(&mut order_fk)
+                    .foreign_key(&mut payment_method_fk)
                     .to_owned(),
             )
             .await
