e24a86a7de5a8cc703e2340c2067dab2acc9aadd
cainhooow <augustocaio663@gmail.com>
2026-03-23 17:51:29 -0300

    UserRepositoryInterface

--- src/domain/repositories/user_repository_interface.rs
+++ src/domain/repositories/user_repository_interface.rs
@@ -1,2 +1,13 @@
+use crate::domain::{
+    builders::user_builder::NewUser, entities::user::User, exceptions::RepositoryError,
+};
+
 #[async_trait::async_trait]
-pub trait UserRepository: Send + Sync {}
+pub trait UserRepository: Send + Sync {
+    async fn find_first(&self) -> Result<User, RepositoryError>;
+    async fn find_by_id(&self, id: &str) -> Result<User, RepositoryError>;
+    async fn find_by_email(&self, email: &str) -> Result<User, RepositoryError>;
+    async fn save(&self, user: &NewUser) -> Result<User, RepositoryError>;
+    async fn delete(&self, id: &str) -> Result<bool, RepositoryError>;
+    async fn update(&self, user: &User) -> Result<User, RepositoryError>;
+}
--- src/infrastructure/persistence/mod.rs
+++ src/infrastructure/persistence/mod.rs
@@ -1,1 +1,2 @@
-
+// rustautomod
+pub mod sea_orm_user_repository;
