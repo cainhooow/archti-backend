6f33479be829c3f9f601565342c53d5f59d16fff
cainhooow <augustocaio663@gmail.com>
2026-03-23 11:34:30 -0300

    company entity model

---
+++ src/infrastructure/entities/company.rs
@@ -0,0 +1,26 @@
+use sea_orm::entity::prelude::*;
+
+#[sea_orm::model]
+#[derive(Debug, Clone, PartialEq, DeriveEntityModel, Eq)]
+#[sea_orm(table_name = "company")]
+pub struct Model {
+    #[sea_orm(primary_key, auto_increment = false)]
+    pub id: Uuid,
+    pub legal_name: String,
+    pub trade_name: String,
+    pub service_type: String,
+    #[sea_orm(unique)]
+    pub document: String,
+    pub contact_name: String,
+    pub primary_phone: String,
+    pub license_plan: String,
+    pub license_status: String,
+    pub license_days_remaining: i32,
+    pub operational_base: String,
+    #[sea_orm(column_type = "Text", nullable)]
+    pub notes: Option<String>,
+    pub created_at: DateTime,
+    pub updated_at: DateTime,
+}
+
+impl ActiveModelBehavior for ActiveModel {}
--- src/infrastructure/entities/mod.rs
+++ src/infrastructure/entities/mod.rs
@@ -1,3 +1,4 @@
 // rustautomod
 pub mod user;
 pub mod company_membership;
+pub mod company;
