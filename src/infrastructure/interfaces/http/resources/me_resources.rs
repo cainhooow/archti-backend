08ee0b745eec436df2156100e14c5870afd7c80d
cainhooow <augustocaio66@gmail.com>
2026-03-29 18:23:01 -0300

    revert serialize to deserialize in change password request

--- src/infrastructure/interfaces/http/resources/me_resources.rs
+++ src/infrastructure/interfaces/http/resources/me_resources.rs
@@ -3,10 +3,10 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Validate)]
 pub struct ChangePasswordRequest {
-    #[serde(rename(serialize = "oldPassword"))]
+    #[serde(rename(deserialize = "oldPassword"))]
     #[garde(ascii)]
     pub old_password: String,
-    #[serde(rename(serialize = "newPassword"))]
+    #[serde(rename(deserialize = "newPassword"))]
     #[garde(ascii)]
     pub new_password: String
 }
