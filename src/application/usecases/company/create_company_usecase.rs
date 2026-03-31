8d6b4f612ad732cf5f7bbd0a8539661387e23b20
cainhooow <augustocaio663@gmail.com>
2026-03-31 12:28:33 -0300

    company validation in usecase

--- src/application/usecases/company/create_company_usecase.rs
+++ src/application/usecases/company/create_company_usecase.rs
@@ -1,14 +1,17 @@
 use crate::{
-    application::exceptions::AppResult,
+    application::exceptions::{AppError, AppResult},
     domain::{
         entities::company::Company,
-        repositories::company_repository_interface::CreateCompanyRepository,
+        repositories::company_repository_interface::{
+            CompanyReadRepository, CreateCompanyRepository,
+        },
+        value_objects::document_vo::Document,
     },
 };
 
 pub struct CreateCompanyUseCase<R>
 where
-    R: CreateCompanyRepository,
+    R: CreateCompanyRepository + CompanyReadRepository,
 {
     repository: R,
 }
@@ -27,18 +30,26 @@ pub struct CreateCompanyCommand {
 
 impl<R> CreateCompanyUseCase<R>
 where
-    R: CreateCompanyRepository,
+    R: CreateCompanyRepository + CompanyReadRepository,
 {
     pub fn new(repository: R) -> Self {
         Self { repository }
     }
 
     pub async fn execute(&self, command: CreateCompanyCommand) -> AppResult<Company> {
+        let document = Document::parse(command.document)?;
+
+        if self.repository.exists_by_document(&document).await? {
+            return Err(AppError::Bad(
+                "There is already a company registered with this information.".to_string(),
+            ));
+        }
+
         let company = Company::register(
             command.legal_name,
             command.trade_name,
             command.service_type,
-            command.document,
+            document.to_string(),
             command.contact_name,
             command.primary_phone,
             command.secondary_phone,
