98c91a9660919d67d01048c58f4c339cef46e03d
cainhooow <augustocaio663@gmail.com>
2026-03-31 19:34:10 -0300

    company router

--- src/infrastructure/interfaces/http/routers/v1/me/company.rs
+++ src/infrastructure/interfaces/http/routers/v1/me/company.rs
@@ -1,5 +1,7 @@
 use salvo::prelude::*;
 
+use crate::infrastructure::interfaces::http::handlers::me::company::create_company_handler::create_company_handler;
+
 pub fn router() -> Router {
-    Router::new()
+    Router::new().post(create_company_handler)
 }
