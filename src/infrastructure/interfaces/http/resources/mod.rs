7c58df9e4c0370a02638af9fcc34cfbe104320d8
cainhooow <augustocaio663@gmail.com>
2026-04-01 01:47:33 -0300

    doc DataResponse Struct

--- src/infrastructure/interfaces/http/resources/mod.rs
+++ src/infrastructure/interfaces/http/resources/mod.rs
@@ -6,7 +6,13 @@ pub mod company_resources;
 pub mod me_resources;
 pub mod message_resource;
 pub mod user_resources;
-
+/*
+* DataResponse is a struct that allows you to standardize API
+* responses and avoid manually defining part-of-the-route responses.
+*
+* DataResponse works in conjunction with resources,
+* which translate domain structures into standardized JSON responses.
+*/
 #[derive(Serialize)]
 pub struct DataResponse<T> {
     pub success: bool,
@@ -14,13 +20,18 @@ pub struct DataResponse<T> {
 }
 
 impl<T> DataResponse<T> {
+    /*
+     * Creates a successful DataResponse with the given data.
+     */
     pub fn success(data: T) -> Self {
         DataResponse {
             success: true,
             data,
         }
     }
-
+    /*
+     * Creates an error DataResponse with the given data.
+     */
     pub fn error(data: T) -> Self {
         DataResponse {
             success: false,
@@ -28,7 +39,10 @@ impl<T> DataResponse<T> {
         }
     }
 }
-
+/*
+ * Implements the Scribe trait for DataResponse,
+ * allowing it to be used as a response body in Salvo.
+ */
 impl<T> salvo::Scribe for DataResponse<T>
 where
     T: Serialize + Send,
