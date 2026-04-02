c9e48e4e006a838013d33ac2d396bd6bc7a1facb
cainhooow <augustocaio663@gmail.com>
2026-04-02 17:31:27 -0300

    role entity model

--- src/infrastructure/entities/mod.rs
+++ src/infrastructure/entities/mod.rs
@@ -3,4 +3,5 @@ pub mod company;
 pub mod company_membership;
 pub mod order;
 pub mod prelude;
+pub mod role;
 pub mod user;
---
+++ src/infrastructure/entities/role.rs
@@ -0,0 +1,18 @@
+use sea_orm::prelude::*;
+
+#[sea_orm::model]
+#[derive(Debug, Clone, PartialEq, DeriveEntityModel, Eq)]
+#[sea_orm(table_name = "roles")]
+pub struct Model {
+    #[sea_orm(primary_key, auto_increment = false)]
+    pub id: Uuid,
+    pub company_id: Uuid,
+    pub code: String,
+    pub name: String,
+    #[sea_orm(column_type = "Text", nullable)]
+    pub description: Option<String>,
+    pub is_system_role: bool,
+    pub created_at: DateTime,
+}
+
+impl ActiveModelBehavior for ActiveModel {}
